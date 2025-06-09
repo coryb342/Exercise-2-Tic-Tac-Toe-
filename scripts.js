a846615d858922b97d26860859548882c853d368
Cory Bateman <coryb342@gmail.com>
2025-06-09 13:09:52 -0700

    Rework game logic and add in highlight of winning combo

--- scripts.js
+++ scripts.js
@@ -5,21 +5,29 @@
  * Description: This script is used for the tick-tac-toe game.
  */
 
-// Game state logic
+
 const human_player = 'X';
 const computer_player = 'O';
 const num_board_spaces = 9;
-const winning_legnth = 3;
-const winning_combos = ["123", "456", "789", "147", "258", "369", "159", "357"];
+const winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 let game_over = false;
 let current_player = computer_player;
 let current_move = 1;
+let comp_held_positions = [];
+let human_held_positions = [];
+let start_clear_button = document.querySelector('#start-clear');
 const board = document.querySelectorAll('.board-space');
 
-
+// Game state logic
 function startGame() {
-    disableBoard();
-    computerMove();
+    if (start_clear_button.value === 'Start') {
+        start_clear_button.value = 'Clear';
+        start_clear_button.disabled = true;
+        computerMove();
+    }
+    else {
+        resetGame();
+    }
 }
 
 document.addEventListener('click', function(event) {
@@ -33,10 +41,12 @@ function playerMove(event) {
     const target = event.target;
     if (target.value === '' && !game_over) {
         target.value = human_player;
-        current_player = computer_player;
-        disableBoard();
-        computerMove();
-        isWinner(human_player);
+        human_held_positions.push(target.id);
+        current_move++;
+        if (isWinner(human_player)) {
+            return;
+        }
+        swapPlayer(human_player);
     }
 }
 
@@ -46,40 +56,76 @@ function computerMove() {
         computer_move = Math.floor(Math.random() * num_board_spaces);
     }
     board[computer_move].value = computer_player;
-    current_player = human_player;
-    
-    if (current_move === 1) {
-        current_move++;
+    comp_held_positions.push(board[computer_move].id);
+    current_move++;
+    if (current_move === 2) {
         computerMove();
     }
-
-    enableBoard();
-    setTimeout(() => {
-        isWinner(computer_player);
-    }, 100);
+    if (isWinner(computer_player)) {
+        return;
+    }
+    swapPlayer(computer_player);
 }
 
 function isWinner(player) {
-    alert(current_move);
-    if (current_move > num_board_spaces) {
-        alert("It's a draw!");
-        game_over = true;
-        return false;
-    }
-    const playerPositions = [];
-    board.forEach((space, index) => {
-        if (space.value === player) {
-            playerPositions.push((index + 1).toString());
+    const held_positions = player === human_player ? human_held_positions : comp_held_positions;
+    for (const combo of winning_combos) {
+        if (combo.every(pos => held_positions.includes(pos.toString()))) {
+            highligtWinningCombo(combo);
+            setGameOver(true, player);
+            return true;
         }
-    });
-    if (winning_combos.some(combo => combo.split('').every(pos => playerPositions.includes(pos)))) {
-        alert(`${player} wins!`);
-        game_over = true;           
     }
-    current_move++;
+    if (current_move > num_board_spaces) {
+        setGameOver(true);
+        return true;
+    }
     return false;
 }
 
+function setGameOver(bool, player = '') {
+    game_over = bool;
+    if (game_over && player === '') {
+        disableBoard(); 
+        start_clear_button.disabled = false; 
+        setTimeout(() => {
+            alert("It's a draw!");;
+        }, 100); 
+        return;
+    }
+    if (game_over && player === human_player) {
+        disableBoard();
+        start_clear_button.disabled = false;
+        setTimeout(() => {
+            alert("You Win!");;
+        }, 100); 
+        return;
+    }
+    if (game_over && player === computer_player) {
+        disableBoard();
+        start_clear_button.disabled = false;  
+        setTimeout(() => {
+            alert("Computer Wins!");;
+        }, 100); 
+        return;
+    }
+    swapPlayer(player);
+}
+
+function swapPlayer(player) {
+    if (player === human_player) {
+        current_player = computer_player;
+        disableBoard();
+        setTimeout(() => {
+            computerMove();
+        }, 500);
+    } else if (player === computer_player) {
+        enableBoard();
+        current_player = human_player; 
+    }
+}
+
+// GUI Logic
 function disableBoard() {
     board.forEach(space => {
         space.disabled = true;
@@ -89,5 +135,30 @@ function disableBoard() {
 function enableBoard() {
     board.forEach(space => {
         space.disabled = false;
+    });
+}
+
+function resetGame() {
+    game_over = false;
+    current_player = computer_player;
+    current_move = 1;
+    comp_held_positions = [];
+    human_held_positions = [];
+    start_clear_button.value = 'Start';
+    start_clear_button.disabled = false;
+    disableBoard();
+    
+    board.forEach(space => {
+        space.value = '';
+        space.classList.remove('winning-space');
+    });
+}
+
+function highligtWinningCombo(combo) {
+    combo.forEach(pos => {
+        const space = document.getElementById(pos);
+        if (space) {
+            space.classList.add('winning-space');
+        }
     });
 }
